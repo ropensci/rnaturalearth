2b8157e7ca271867bfbc221fbf45c6096db5c014
AndySouth <southandy@gmail.com>
2015-07-22 23:36:26 +0100

    start datasets doc

---
+++ R/datasets.r
@@ -0,0 +1,14 @@
+#' world country polygons from Natural Earth at 1:110m (small) resolution
+#'
+#' boundaries of world countries and associated attribute data
+#'
+#' @slot data A data frame with country attributes 
+#'
+#' @format A \code{SpatialPolygonsDataFrame} with 177 obs. of  63 variables
+#' @docType data
+#' @keywords dataset
+#' @source \url{http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_countries.zip}
+#'
+#' @usage data(countries110)
+"countries110"
+
