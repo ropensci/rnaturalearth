f28af9e9f83f88280509e694e49b4feb9a1925dd
AndySouth <southandy@gmail.com>
2015-08-06 13:13:12 +0100

    add datasets

--- R/datasets.r
+++ R/datasets.r
@@ -26,6 +26,20 @@
 #' @usage data(countries50)
 "countries50"
 
+#' world country polygons from Natural Earth at 1:10m (large) resolution
+#'
+#' boundaries of world countries and associated attribute data
+#'
+#' @slot data A data frame with country attributes 
+#'
+#' @format A \code{SpatialPolygonsDataFrame} with 255 elements
+#' @docType data
+#' @keywords dataset
+#' @source \url{http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_countries.zip}
+#'
+#' @usage data(countries10)
+"countries10"
+
 #' world map_unit polygons from Natural Earth at 1:110m (small) resolution
 #'
 #' there are more map_units than countries e.g. French Guiana is separate from France
@@ -40,6 +54,34 @@
 #' @usage data(map_units110)
 "map_units110"
 
+#' world map_unit polygons from Natural Earth at 1:50m (medium) resolution
+#'
+#' there are more map_units than countries e.g. French Guiana is separate from France
+#'
+#' @slot data A data frame with attributes 
+#'
+#' @format A \code{SpatialPolygonsDataFrame} with 264 elements
+#' @docType data
+#' @keywords dataset
+#' @source \url{http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_map_units.zip}
+#'
+#' @usage data(map_units50)
+"map_units50"
+
+#' world map_unit polygons from Natural Earth at 1:10m (large) resolution
+#'
+#' there are more map_units than countries e.g. French Guiana is separate from France
+#'
+#' @slot data A data frame with attributes 
+#'
+#' @format A \code{SpatialPolygonsDataFrame} with 295 elements
+#' @docType data
+#' @keywords dataset
+#' @source \url{http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_map_units.zip}
+#'
+#' @usage data(map_units10)
+"map_units10"
+
 #' world sovereignty polygons from Natural Earth at 1:110m (small) resolution
 #'
 #' there are fewer sovereign states than countries e.g. the Falkland Islands are included with the United Kingdom
@@ -53,6 +95,34 @@
 #'
 #' @usage data(sovereignty110)
 "sovereignty110"
+
+#' world sovereignty polygons from Natural Earth at 1:50m (medium) resolution
+#'
+#' there are fewer sovereign states than countries e.g. the Falkland Islands are included with the United Kingdom
+#'
+#' @slot data A data frame with attributes 
+#'
+#' @format A \code{SpatialPolygonsDataFrame} with 200 elements
+#' @docType data
+#' @keywords dataset
+#' @source \url{http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_sovereignty.zip}
+#'
+#' @usage data(sovereignty50)
+"sovereignty50"
+
+#' world sovereignty polygons from Natural Earth at 1:10m (large) resolution
+#'
+#' there are fewer sovereign states than countries e.g. the Falkland Islands are included with the United Kingdom
+#'
+#' @slot data A data frame with attributes 
+#'
+#' @format A \code{SpatialPolygonsDataFrame} with 206 elements
+#' @docType data
+#' @keywords dataset
+#' @source \url{http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_sovereignty.zip}
+#'
+#' @usage data(sovereignty10)
+"sovereignty10"
 
 
 #' world state (admin level 1) polygons from Natural Earth at 1:10m (high) resolution
