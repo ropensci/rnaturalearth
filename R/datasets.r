f273bba491d1bfab13aae1ce85ce712ae5e6b328
AndySouth <southandy@gmail.com>
2015-08-06 18:59:25 +0100

    put datasets doc back into datasets.r

--- R/datasets.r
+++ R/datasets.r
@@ -1,3 +1,50 @@
+#' world country polygons from Natural Earth 
+#'
+#' at scales 1:110m (small), 1:50m (medium), 1:10m (large)
+#'
+#' @format A \code{SpatialPolygonsDataFrame}
+#' @slot data A data frame with country attributes 
+#' @name countries
+NULL
+
+#' @source \url{http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_countries.zip}
+#' @rdname countries
+"countries110"
+
+#' @source \url{http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip}
+#' @rdname countries
+"countries50"
+
+#' @source \url{http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_countries.zip}
+#' @rdname countries
+"countries10"
+
+#' world map_unit polygons from Natural Earth 
+#'
+#' map_unit polygons at scales 1:110m (small), 1:50m (medium), 1:10m (large).   
+#' There are more map_units than countries e.g. United Kingdom is split into England, Scotland, Wales and Northern Ireland.
+#' 
+#' @format A \code{SpatialPolygonsDataFrame}
+#' @slot data A data frame with attributes 
+#' @name map_units
+#' @aliases map_units110 map_units50 map_units10
+NULL
+
+
+#' @source \url{http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_map_units.zip}
+#' @rdname map_units
+"map_units110"
+
+
+#' @source \url{http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_map_units.zip}
+#' @rdname map_units
+"map_units50"
+
+
+#' @source \url{http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_map_units.zip}
+#' @rdname map_units
+"map_units10"
+
 #' world sovereignty polygons from Natural Earth 
 #'
 #' sovereignty polygons at scales 1:110m (small), 1:50m (medium), 1:10m (large).   
