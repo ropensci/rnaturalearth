b17f23276fc3375f1b0c1addb7c891dab83bc6b4
AndySouth <southandy@gmail.com>
2015-08-06 17:55:12 +0100

    split out countries dataset documentation into a separate file

---
+++ R/countries.r
@@ -0,0 +1,20 @@
+#' world country polygons from Natural Earth 
+#'
+#' at scales 1:110m (small), 1:50m (medium), 1:10m (large)
+#'
+#' @format A \code{SpatialPolygonsDataFrame}
+#' @slot data A data frame with country attributes 
+#' @name countries
+NULL
+
+#' @source \url{http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_countries.zip}
+#' @rdname countries
+"countries110"
+
+#' @source \url{http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip}
+#' @rdname countries
+"countries50"
+
+#' @source \url{http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_countries.zip}
+#' @rdname countries
+"countries10"
