a3cc32c7720703424a31ce67e1da9ca358e2fb4a
AndySouth <southandy@gmail.com>
2017-02-12 17:42:15 +0000

    dodgy ascii conversion commented now done in ne_download

--- data-raw/data_download_script.r
+++ data-raw/data_download_script.r
@@ -1,8 +1,10 @@
 #data-raw/data_download_script.r
 
-#how data is got into rnaturalearth
+#getting data into rnaturalearth for admin
+#NOT for users
 #reproducible workflow allowing package data to be updated e.g. when there are updates to Natural Earth. 
 #just by sourcing this script
+#need also to source the same named script in rnaturalearthdata and rnaturalearthhires
 
 #one example file in rnaturalearth
 #scale 110 and 50 files in rntauralearthdata
@@ -15,21 +17,22 @@ countries110 <- ne_download(scale=110, type='countries', category='cultural')
 #to allow same operation on all data objects in the package
 data_object_names <- data(package = "rnaturalearth")[["results"]][,"Item"]
 
+# 12/2/17 below replaced with rgdal::readOGR(iconv=TRUE) in ne_download() 
 #dangerous code to remove all non-ascii characters
 #todo I might want to do something more intelligent than this
 #e.g. currently Curaçao is converted to Curaao
-for (i in 1:length(data_object_names))
-{
-  data_name <- data_object_names[i]
-  data_object <- eval(parse(text = data_name))
-  
-  data_object@data <- data.frame( lapply(data_object@data, function(x) iconv(x, "latin1", "ASCII", sub="")))
-  
-  #but now I want to reallocate back to the named object
-  #beware this is dangerous
-  eval(parse(text=paste(data_name,"<- data_object")))
-  
-}
+# for (i in 1:length(data_object_names))
+# {
+#   data_name <- data_object_names[i]
+#   data_object <- eval(parse(text = data_name))
+#   
+#   #BEWARE if go back to this new stringsAsFactors=FALSE converts all fields to 'chr'
+#   data_object@data <- data.frame( lapply(data_object@data, function(x) iconv(x, "latin1", "ASCII", sub="")), stringsAsFactors=FALSE)
+#   
+#   #but now I want to reallocate back to the named object
+#   #beware this is dangerous
+#   eval(parse(text=paste(data_name,"<- data_object")))
+# }
 
 #### saving data files to correct folder in the package
 # relies on working directory being set to root of the package
