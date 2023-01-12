dfb0accc77c130f89f97e57f2427f3842137ced9
Philippe Massicotte <pmassicotte@hotmail.com>
2023-01-12 11:44:34 -0500

    Fix broken url in documentation.

--- R/datasets.r
+++ R/datasets.r
@@ -1,39 +1,38 @@
-#' world country polygons from Natural Earth 
+#' world country polygons from Natural Earth
 #'
 #' at 1:110m scale (small). Other data and resolutions are in the packages rnaturalearthdata and rnaturalearthhires.
 #'
 #' @format A \code{SpatialPolygonsDataFrame}
-#' @slot data A data frame with country attributes 
+#' @slot data A data frame with country attributes
 #' @aliases countries110
 #' @name countries
 NULL
 
-#' @source \url{http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_countries.zip}
+#' @source \url{https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_countries.zip}
 #' @rdname countries
 "countries110"
 
-#' list of cultural layers available from Natural Earth 
+#' list of cultural layers available from Natural Earth
 #'
 #'
 #' @format A \code{DataFrame}
 #' @aliases df_layers_cultural
 #' @name df_layers_cultural
-#' 
+#'
 NULL
 
 #' @rdname df_layers_cultural
 "df_layers_cultural"
 
 
-#' list of physical layers available from Natural Earth 
+#' list of physical layers available from Natural Earth
 #'
 #'
 #' @format A \code{DataFrame}
 #' @aliases df_layers_physical
 #' @name df_layers_physical
-#' 
+#'
 NULL
 
 #' @rdname df_layers_physical
 "df_layers_physical"
-
