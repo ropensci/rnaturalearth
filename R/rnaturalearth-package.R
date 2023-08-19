7191a22b8277a31caeef944fcbe78367ebdfeca5
Philippe Massicotte <pmassicotte@hotmail.com>
2023-08-19 07:14:30 -0400

    fixing package namespace (#85)

--- R/rnaturalearth-package.R
+++ R/rnaturalearth-package.R
@@ -1,20 +1,11 @@
-#' rnaturalearth : world map data from Natural Earth
-#'
-#' Facilitates world mapping by making
-#' \href{https://www.naturalearthdata.com/}{Natural Earth} map data more easily
-#' available to R users.
-#'
-#' @name rnaturalearth
+#' @keywords internal
+"_PACKAGE"
 
-#' @docType package
-
-#' @seealso \code{\link{ne_countries}} \code{\link{ne_states}}
-#' \code{\link{ne_download}}
-
-#' @import sp utils sf
+## usethis namespace: start
+#' @import sp
+#' @importFrom sf as_Spatial
+#' @importFrom utils read.csv
+#' @import httr
+#' @importFrom jsonlite fromJSON
+## usethis namespace: end
 NULL
-
-# Hide variables from R CMD check
-if (getRversion() >= "2.15.1") {
-  utils::globalVariables(c("countries110"))
-}
