13371c0b28f2f10dd402454a765fe9faa7273771
Philippe Massicotte <pmassicotte@hotmail.com>
2025-07-13 14:00:54 -0400

    fixup! test: replace inherits() with more specific expect_s3_class and expect_s4_class assertions

--- tests/testthat/test_ne_load.R
+++ tests/testthat/test_ne_load.R
@@ -3,19 +3,24 @@ test_that("ne_load works for raster files", {
   skip_if_not_installed("rnaturalearthdata")
   skip_if_not_installed("rnaturalearthhires")
 
+  scale <- 50L
+  type <- "OB_50M"
+  category <- "raster"
+  tmpdir <- tempdir()
+
   ne_download(
-    scale = 50L,
-    type = "OB_50M",
-    category = "raster",
-    destdir = tempdir(),
+    scale = scale,
+    type = type,
+    category = category,
+    destdir = tmpdir,
     load = FALSE
   )
 
   rst <- ne_load(
-    scale = 50L,
-    type = "OB_50M",
-    category = "raster",
-    destdir = tempdir()
+    scale = scale,
+    type = type,
+    category = category,
+    destdir = tmpdir
   )
 
   expect_s4_class(rst, "SpatRaster")
@@ -26,23 +31,26 @@ test_that("ne_load works for vector files", {
   skip_if_not_installed("rnaturalearthdata")
   skip_if_not_installed("rnaturalearthhires")
 
+  tmpdir <- tempdir()
+
+  scale_small <- 50L
+
   ne_download(
-    scale = 50L,
-    destdir = tempdir(),
+    scale = scale_small,
+    destdir = tmpdir,
     load = FALSE
   )
 
-  vect <- ne_load(scale = 50L, destdir = tempdir(), returnclass = "sv")
+  vect <- ne_load(scale = scale_small, destdir = tmpdir, returnclass = "sv")
   expect_s4_class(vect, "SpatVector")
 
-  vect <- ne_load(scale = 50L, destdir = tempdir(), returnclass = "sf")
+  vect <- ne_load(scale = scale_small, destdir = tmpdir, returnclass = "sf")
   expect_s3_class(vect, "sf")
 
-  tmpdir <- tempdir()
-  scale <- "medium"
+  scale_medium <- "medium"
 
   ne_download(
-    scale = scale,
+    scale = scale_medium,
     type = "land",
     category = "physical",
     destdir = tmpdir,
@@ -50,7 +58,7 @@ test_that("ne_load works for vector files", {
   )
 
   vect <- ne_load(
-    scale = scale,
+    scale = scale_medium,
     type = "land",
     category = "physical",
     destdir = tmpdir
